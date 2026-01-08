610d6de85bc874f7c603bc7fb15a6291ae315957
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-01-08 11:35:33 +0530

    vite commit

--- responsive-product-gallery/vite.config.js
+++ responsive-product-gallery/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Week-5-Day-1-Task/",
+  base: "/Week-5-Day-2-Task/",
 });
